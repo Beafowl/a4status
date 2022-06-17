8ac29e58328fb4544265fa8f7be3ea88a56a5767
berkbuturak <berkbuturak@gmail.com>
2022-06-17 19:52:46 +0200

    Added channel id to .env, resize debug output, output to textchannel

--- bot.py
+++ bot.py
@@ -11,6 +11,8 @@ import cv2
 config = load_dotenv(find_dotenv())
 client = discord.Client()
 
+channel_to_notify = str(os.environ.get("CHANNEL"))
+
 angels_raid_timestamp = datetime.now()
 demons_raid_timestamp = datetime.now()
 
@@ -26,11 +28,13 @@ async def check_for_raid_angels():
             if a == -1:
                 print("Raid")
                 # send notification to discord server
-                channel = client.get_channel(134343345815289856)
+                channel = client.get_channel(channel_to_notify)
                 await channel.send("Engel haben jetzt Raid!")
                 angels_raid_timestamp = datetime.now()
                 time.sleep(216000)
                 continue
+            else:
+                print("Currently checking for angels raid.. it is at " + str(a) + "%")
         time.sleep(60)
 
 async def check_for_raid_demons():
@@ -42,11 +46,13 @@ async def check_for_raid_demons():
             if d == -1:
                 print("Raid")
                 # send notification to discord server
-                channel = client.get_channel(134343345815289856)
+                channel = client.get_channel(channel_to_notify)
                 await channel.send("Dämonen haben jetzt Raid!")
                 demons_raid_timestamp = datetime.now()
                 time.sleep(216000)
                 continue
+            else:
+                print("Currently checking for demons raid.. it is at " + str(d) + "%")
         time.sleep(60)
 
 @client.event
@@ -74,13 +80,24 @@ async def on_message(message):
             else:
                 demons_text = "Die Dämonen sind bei " + str(d) + "%."
 
-            print(angels_text)
-            print(demons_text)
+            #print(angels_text)
+            #print(demons_text)
+            await message.channel.send(angels_text)
+            await message.channel.send(demons_text)
 
     elif message.content == "!verify":
         angel_percent = cv2.imread("./output/angel.png")
         demon_percent = cv2.imread("./output/demon.png")
         con = cv2.hconcat([angel_percent, demon_percent])
+
+        scale_percent = 400 # percent of original size
+        width = int(con.shape[1] * scale_percent / 100)
+        height = int(con.shape[0] * scale_percent / 100)
+        dim = (width, height)
+  
+        # resize image
+        con = cv2.resize(con, dim, interpolation = cv2.INTER_AREA)
+
         cv2.imwrite("./output/debug.png", con)
         await message.channel.send(file=discord.File('./output/debug.png'))
 
