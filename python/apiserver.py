c4487dcd4152109d2111570790bc57372762a4d0
Beafowl <berkbuturak@gmail.com>
2023-01-10 05:07:01 +0100

    Moved the webpage to the default folders static and templates for flask, added a decrementing progress bar when 100% has been reached

--- python/apiserver.py
+++ python/apiserver.py
@@ -1,13 +1,12 @@
 from detect import angel_status, demon_status, detect_raid_angel, detect_raid_demon
 from mss import mss
-from flask import Flask
+from flask import Flask, render_template
 import json
-from flask import jsonify
 
 HOST = "0.0.0.0"
 PORT = 80
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static')
 
 
 @app.route('/a4status')
@@ -19,6 +18,10 @@ def index():
     f.close()
     return a4status
 
+@app.route('/')
+def home():
+    return render_template('index.html')
+    
 
 if __name__ == "__main__":
     app.run(host=HOST, port=PORT)
---
+++ python/static/script.js
@@ -0,0 +1,76 @@
+const progressBarAngels = document.getElementsByClassName('progress-bar')[0]
+const progressBarDemons = document.getElementsByClassName('progress-bar')[1]
+
+const reqURL = 'http://beafowl.de/a4status';
+
+// get values from the api server
+const getData = async () => {
+
+    d = await fetch(reqURL, { mode: 'same-origin' })
+    .then((response) => response.json())
+    .then((data) => {
+
+        return data;
+    });
+    return d;
+}
+
+// applies the new values
+const updateWebpage = (dataObject) => {
+
+    console.log(dataObject)
+
+    // check if 100 percent has not been reached yet
+    // if one has reached 100%, change the bar to a 'minutes left' bar that decrements
+
+    if (dataObject.angels.percentage != 100) {
+
+        progressBarAngels.style.setProperty('--width', dataObject.angels.percentage);
+        progressBarAngels.setAttribute('data-label', `${dataObject.angels.percentage}%`)
+        progressBarAngels.style.setProperty('--gradientColor1', '#af99b0')
+        progressBarAngels.style.setProperty('--gradientColor2', '#bfacc0')
+
+    } else {
+
+        dataObject.angels.percentage = (dataObject.angels.closes_in / 60) * 100;
+        progressBarAngels.style.setProperty('--width', dataObject.angels.percentage);
+        progressBarAngels.style.setProperty('--gradientColor1', '#ff9400')
+        progressBarAngels.style.setProperty('--gradientColor2', '#ffa200')
+        // background-image: linear-gradient(#af99b0, #bfacc0);
+    }
+
+    if (dataObject.demons.percentage != 100) {
+
+        progressBarDemons.style.setProperty('--width', dataObject.demons.percentage);
+        progressBarDemons.setAttribute('data-label', `${dataObject.demons.percentage}%`)
+        progressBarDemons.style.setProperty('--gradientColor1', '#af99b0')
+        progressBarDemons.style.setProperty('--gradientColor2', '#bfacc0')
+    } else {
+
+        dataObject.demons.percentage = (dataObject.demons.closes_in / 60) * 100;
+        progressBarDemons.style.setProperty('--width', dataObject.demons.percentage);
+        progressBarDemons.style.setProperty('--gradientColor1', '#ff9400')
+        progressBarDemons.style.setProperty('--gradientColor2', '#ffa200')
+    }
+}
+
+// fetch new data from the api server and update webpage
+const update = async () => {
+
+    data = await getData()
+    updateWebpage(data);
+}
+
+// update when webpage will be loaded
+update();
+
+// then every 29 seconds
+setInterval(async () => {
+
+    // set values when the webpage will be loaded
+
+    await update();
+
+
+
+}, 29 * 1000)
