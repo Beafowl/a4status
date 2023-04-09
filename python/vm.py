77074b7d0d8ff1f0ede53235600c2b6f7b057931
Berk Buturak <berkbuturak@gmail.com>
2023-04-09 14:43:04 +0200

    Added commands for going to the game

--- python/vm.py
+++ python/vm.py
@@ -2,10 +2,27 @@ from pywinauto import findwindows, keyboard, Application, mouse
 from flask import Flask
 import flask
 import win32api
+import time
+
+# this code will be run on the virtual machine
+# create a an api server to control the state of the game
+
+# positions of the mouse
+
+SELECT_SERVER = (283, 473)
+SELECT_CHANNEL_7 = (443, 704)
+SELECT_CHARACTER = (56, 205)
+CLOSE_NOSMALL = (735, 953)
+
+# estimated times for transitioning to the next state (in seconds)
+
+TIME_START_NOSTALE = 20
+TIME_TO_CHARACTER_SELECTION = 3
+TIME_TO_GAME = 5
 
 # pywinauto only works for windows
 
-HOST = '127.0.0.1'
+HOST = '192.168.178.141'
 PORT = 5555
 
 app = Flask(__name__)
@@ -44,9 +61,21 @@ def start_nostale():
 
 @app.route('/goto_game')
 def goto_game():
-    pass
+    mouse.click(button='left', coords=SELECT_SERVER)
+    mouse.click(button='left', coords=SELECT_CHANNEL_7)
+
+    time.sleep(TIME_TO_CHARACTER_SELECTION)
+
+    mouse.double_click(button='left', coords=SELECT_CHARACTER)
+
+    time.sleep(TIME_TO_GAME)
+
+    mouse.click(button='left', coords=CLOSE_NOSMALL)
+
     return "Server has been selected"
 
+# this is needed in development in order to find out mouse positions for
+# the automated script
 @app.route('/get_mouse_position')
 def get_mouse_position():
     x, y = win32api.GetCursorPos()
